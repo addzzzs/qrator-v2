d96fbc4c8955f2fb9a4666d24c6afd6c72fcf5c1
John Rhey Dahil <johnrheydahil2003@gmail.com>
2026-08-24 00:57:08 +0800

    update api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://qrator-server.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
